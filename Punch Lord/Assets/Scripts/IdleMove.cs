226b2388905d03183e14f995b2596262154fb150
charles <renaudcharles14@hotmail.com>
2020-02-28 22:06:26 -0500

    Prefabs interactible

---
+++ Punch Lord/Assets/Scripts/IdleMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleMove : MonoBehaviour
+{
+
+    public int translateX, translateY;
+    public Interactible inter;
+    public float moveTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartMove();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(transform.position + new Vector3(translateX, translateY, 0) * moveTime * Time.deltaTime);
+    }
+
+    
+    IEnumerator StartMove()
+    {
+
+        yield return new WaitForSeconds(1);
+        //MoveToTarget(this.transform.position + new Vector3(-translateX, -translateY, 0));
+        transform.Translate((transform.position + new Vector3(translateX, translateY, 0))*Time.deltaTime);
+    }
+
+    IEnumerator StartMoveRight()
+    {
+        yield return new WaitForSeconds(1);
+        MoveToTarget(this.transform.position + new Vector3(translateX,translateY,0));
+    }
+
+    void MoveToTarget(Vector3 targetPosition)
+    {
+        Vector3 currentPosition = this.transform.position;
+        
+            Vector3 directionOfTravel = targetPosition - currentPosition;
+            directionOfTravel.Normalize();
+            this.transform.Translate(
+                            (directionOfTravel.x * inter.idleSpeed * Time.deltaTime),
+                            (directionOfTravel.y * inter.idleSpeed * Time.deltaTime),
+                            (directionOfTravel.z * inter.idleSpeed * Time.deltaTime),
+                            Space.World);
+    }
+}
---
+++ Punch Lord/Assets/Scripts/Interactible.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible : MonoBehaviour
+{
+    public float idleSpeed;
+    public float lives;
+    public float bounceability;
+    //public Player player;
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    /*
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (player.isAttacking())
+        {
+            lives--;
+        }
+    }
+    */
+
+    private void FixedUpdate()
+    {
+        if (lives == 0)
+        {
+            DisableInstance();
+        }
+    }
+
+    void DisableInstance()
+    {
+        // Removes this script instance from the game object
+        this.enabled = false;
+    }
+}
