c840a21a1583c0f0ba4e2cf94ca43d6424b38f0b
Maxime <wixxwistiii@gmail.com>
2020-03-01 00:57:12 -0500

    idk either charles

--- Punch Lord/Assets/Poing.cs
+++ Punch Lord/Assets/Poing.cs
@@ -4,44 +4,45 @@ using UnityEngine;
 
 public class Poing : MonoBehaviour
 {
-    public GameObject main;
-    public Transform target;
     public float speed;
-    public Sprite spriteNeutral;
     public Animator animator;
     public Vector3 start;
-    public bool move;
-    float t;
 
     public void onPunch()
     {
-        move = false;
-        transform.position = start;
-        IEnumerator Stop()
-        {
-            yield return new WaitForSeconds(0.5009f);
-            animator.Play("poingStill");
-            move = false;
-            transform.position = start;
-        }
+        start = gameObject.transform.position;
         StopCoroutine(Stop());
         animator.Play("poingStill");
         animator.Play("frapper");
         StartCoroutine(Stop());
         start = transform.position;
-        move = true;
         //animator.SetBool("isAttacking", true);
         //StartCoroutine(animPoing());
 
     }
 
+    IEnumerator Stop()
+    {
+        while (gameObject.transform.position.y < GameObject.Find("HitBox").transform.position.y + GameObject.Find("HitBox").GetComponent<CapsuleCollider2D>().size.y)
+        {
+            Vector2 aim = new Vector2(Input.mousePosition.x - transform.position.x, Input.mousePosition.y - transform.position.y);
+            float angle = Mathf.Atan2(-aim.y, -aim.x) * Mathf.Rad2Deg;
+            Debug.Log("hello");
+            gameObject.transform.position = new Vector2(gameObject.transform.position.y + speed, gameObject.transform.position.x);
+        }
+        yield return new WaitForSeconds(0.5009f);
+        animator.Play("poingStill");
+
+        gameObject.transform.position = start;
+    }
+
     private void Update()
     {
-        if (move)
+        /*if (move)
         {
             transform.position += Vector3.Lerp(start, target.position, t);
             t += 1f * Time.deltaTime;
-        }
+        }*/
 
     }
 
