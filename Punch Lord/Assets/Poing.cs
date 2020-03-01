09c0e13dbecbb02c687a8bb9556417fd935075c1
charles <renaudcharles14@hotmail.com>
2020-02-29 23:25:04 -0500

    max its for you

--- Punch Lord/Assets/Poing.cs
+++ Punch Lord/Assets/Poing.cs
@@ -4,37 +4,46 @@ using UnityEngine;
 
 public class Poing : MonoBehaviour
 {
-    
     public GameObject main;
-    public Vector3 target;
+    public Transform target;
     public float speed;
     public Sprite spriteNeutral;
     public Animator animator;
-
+    public Vector3 start;
+    public bool move;
+    float t;
 
     public void onPunch()
     {
-        animator.SetBool("isAttacking", true);
-        StartCoroutine(animPoing());
+        move = false;
+        transform.position = start;
+        IEnumerator Stop()
+        {
+            yield return new WaitForSeconds(0.5009f);
+            animator.Play("poingStill");
+            move = false;
+            transform.position = start;
+        }
+        StopCoroutine(Stop());
+        animator.Play("poingStill");
+        animator.Play("frapper");
+        StartCoroutine(Stop());
+        start = transform.position;
+        move = true;
+        //animator.SetBool("isAttacking", true);
+        //StartCoroutine(animPoing());
 
-    }
-
-    IEnumerator animPoing()
-    {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target, step);
-        yield return null;
     }
 
     private void Update()
     {
-        target = main.transform.position;
+        if (move)
+        {
+            transform.position += Vector3.Lerp(start, target.position, t);
+            t += 1f * Time.deltaTime;
+        }
+
     }
 
-    public void StopAnim()
-    {
-        StopCoroutine(animPoing());
-        animator.SetBool("isAttacking", false);
-    }
-    
+
 }
